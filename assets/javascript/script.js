be249d66ed5dddd3ebc1cfbc7b99c797c6dd26b2
Luke Keltner <lkeltner87@gmail.com>
2017-08-14 23:51:28 -0400

    added trending button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() 
 {
 
-	var resultNumber = 2
+	var resultNumber = 20
+	var trending = false;
 	var searchButtons = $('.search-buttons')
 	var searchResults = $('.search-results')
 
@@ -11,29 +12,31 @@ $(document).ready(function()
 		searchButtons.append(newButton)
 	}
 
-	$('.btn').on('click', function()
-	{
-		var userInput = $('input').val()
-		getNewButton(userInput)
-	})	
-
-	searchButtons.on('click', function(event)
+	var getSearchResults = function(searchString, trending)
 	{
 		searchResults.empty()
-		var searchString = event.target.textContent
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q="+searchString+"&api_key=dc6zaTOxFJmzC&limit="+resultNumber;
+
+		if (trending)
+		{
+			var queryURL = "http://api.giphy.com/v1/gifs/trending?&api_key=dc6zaTOxFJmzC&limit="+resultNumber;
+		}
+
+		else
+		{
+			var queryURL = "http://api.giphy.com/v1/gifs/search?q="+searchString+"&api_key=dc6zaTOxFJmzC&limit="+resultNumber;
+		}
 
 	    $.ajax(
 	    {
 		    url: queryURL,
 		    method: 'GET'
+
 	    }).done(function(response) 
 	    {
 	    	console.log(response)
 
 		    for (var i=0; i<response.data.length; i++)
 		    {
-			    console.log('HI!')
 				var newImg = $('<img>')
 				var stillImg = response.data[i].images.original_still.url
 				var moveImg = response.data[i].images.downsized.url
@@ -45,11 +48,27 @@ $(document).ready(function()
 				newImg.data('moving', false)
 				searchResults.append(newImg)
 			}
-
-
-	      //data = JSON.parse(response.responseText).data.image_url
-	     //$('.pic').attr('src', data)
 	    });
+	}
+
+	$('#search-button').on('click', function()
+	{
+		var userInput = $('input').val()
+		if (userInput!=="")
+		{
+			getNewButton(userInput)
+		}
+	})
+
+	$('#trending-button').on('click', function()
+	{
+		getSearchResults("", true)
+	})
+
+	searchButtons.on('click', function(event)
+	{
+		var searchString = event.target.textContent
+		getSearchResults(searchString, false)
 
 	})
 
@@ -74,9 +93,9 @@ $(document).ready(function()
 				{
 					var stopData = imgClicked.data('stop')
 					imgClicked.attr('src', stopData)
+					imgClicked.data('moving', false)
 				}
 			}
 		}
 	})
-
 });
